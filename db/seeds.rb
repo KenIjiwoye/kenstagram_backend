3225a1179d7e4d56a6938de0a3cf9ea171c4228e
KenIjiwoye <alistluxury@gmail.com>
2022-02-25 16:49:27 +0000

    add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# (1..10).each do 
+#     User.create!(
+#         email: Faker::Internet.email,
+#         password: "password",
+#         password_confirmation: "password",
+#         username: Faker::Lorem.word
+#     )
+# end
+
+# (1..10).each do 
+#     Post.create!(
+#         caption: Faker::Lorem.sentence,
+#         user_id: rand(1..10)
+#     )
+# end
+
+# (1..10).each do 
+#     Comment.create!(
+#         text: Faker::Lorem.sentence,
+#         user_id: rand(1..10),
+#         post_id: rand(1..10)
+#     )
+# end
