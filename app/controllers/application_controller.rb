dd7f03ddf28f1051c32e72277460047fd6385a91
KenIjiwoye <alistluxury@gmail.com>
2022-03-15 21:25:49 +0000

    code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::API
-    # include ActionController::Cookies
-    # include ActionController::RequestForgeryProtection
 
-    # protect_from_forgery with: :null_session
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
